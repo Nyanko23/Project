438b0886d7bdede1defa1aad7148a2ace9f30ae8
Nyanko23 <117894617+Nyanko23@users.noreply.github.com>
2023-02-16 13:01:37 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -1,4 +1,8 @@
+#ASP Project
 import pandas as pd
+#reading and storing csv to dataframe
 monthly_df = pd.read_excel("Project_File.xlsx")
 print(monthly_df)
+#getting the summary of df
 a= monthly_df.info(verbose = True)
+print(a)
