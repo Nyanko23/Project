873e5e717673d1f64be5c80d31fe4598a40ca7de
Nyanko23 <117894617+Nyanko23@users.noreply.github.com>
2023-02-16 13:00:19 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -1,5 +1,4 @@
-print("hello")
-print("asshole")
 import pandas as pd
-monthly_df = pd.read_csv("Project_File.xlsx")
+monthly_df = pd.read_excel("Project_File.xlsx")
 print(monthly_df)
+a= monthly_df.info(verbose = True)
