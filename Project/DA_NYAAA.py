7f6a34327cae8edbc923b11182cb7764a954a588
Nyanko23 <117894617+Nyanko23@users.noreply.github.com>
2023-02-17 20:13:04 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -1,3 +1,4 @@
+#ASP Project
 import pandas as pd
 #reading and storing csv to dataframe
 country_tourist_df = pd.read_excel("Project_File.xlsx")
@@ -7,3 +8,6 @@ a= country_tourist_df.info(verbose = True)
 print(a)
 #check the df datatype
 print(country_tourist_df.dtypes)
+#rename the first column
+country_tourist_df.columns.values[0] = "year_month"
+print(country_tourist_df)
