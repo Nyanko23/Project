e63be5393baa0d465cbbf47bc0fa3b48900291c7
Nyanko23 <117894617+Nyanko23@users.noreply.github.com>
2023-02-17 21:49:21 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -1,14 +1,30 @@
 #ASP Project
 import pandas as pd
+import numpy as np
+
 #reading and storing csv to dataframe
 country_tourist_df = pd.read_excel("Project_File.xlsx")
 print(country_tourist_df.head())
+
 #getting the summary of df
 a= country_tourist_df.info(verbose = True)
 print(a)
+
 #check the df datatype
 print(country_tourist_df.dtypes)
+
 #rename the first index
 country_tourist_df.columns.values[0] = "year_month"
 print(country_tourist_df)
-#using str.split on year_month column and store it as year, month
+
+#changing the datatype
+
+
+
+#splitting the year and month different
+#date = country_tourist_df["year_month"].str.split(' ', n = 2, expand = True)
+
+#country_tourist_df["year"] = date[1]
+#country_tourist_df["month"] = date[2]
+
+#country_tourist_df.head()
