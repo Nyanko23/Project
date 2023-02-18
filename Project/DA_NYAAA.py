4755e9a74bb5bead6f85c895d4bf438fbad7883e
Nyanko23 <117894617+Nyanko23@users.noreply.github.com>
2023-02-18 21:14:56 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #reading and storing csv to dataframe
 country_tourist_df = pd.read_excel("Project_File.xlsx")
-print(country_tourist_df.head()) #to view first few records
+print(country_tourist_df.head()) #view the first few records
 
 #getting the summary of df
 a= country_tourist_df.info(verbose = True)
@@ -13,19 +13,12 @@ print(a)
 #check the df datatype
 print(country_tourist_df.dtypes)
 
-#rename the first index
-country_tourist_df.columns.values[0] = "year_month"
-print(country_tourist_df)
-
 #changing the datatype
 for i in range(1, len(country_tourist_df.columns)):
     col = country_tourist_df.columns[i]
     country_tourist_df.iloc[:, i] = pd.to_numeric(country_tourist_df.iloc[:, i], errors='coerce').fillna(0).astype('Int64')
+print(country_tourist_df.dtypes) #check the updated datatypes
 
-#splitting the year and month different
-#date = country_tourist_df["year_month"].str.split(' ', n = 2, expand = True)
-
-#country_tourist_df["year"] = date[1]
-#country_tourist_df["month"] = date[2]
-
-#country_tourist_df.head()
+#rename the first index
+country_tourist_df.columns.values[0] = "year_month"
+print(country_tourist_df)
