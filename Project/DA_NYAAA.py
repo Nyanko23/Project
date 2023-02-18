6e3de9c554da09a8cb0a454d7eab6b30d5cacb78
sakuriee <117894686+sakuriee@users.noreply.github.com>
2023-02-18 22:38:00 +0800

    Update DA_NYAAA.py

--- Project/DA_NYAAA.py
+++ Project/DA_NYAAA.py
@@ -4,7 +4,13 @@ import numpy as np
 
 #reading and storing csv to dataframe
 country_tourist_df = pd.read_excel("Project_File.xlsx")
-print(country_tourist_df.head()) #view the first few records
+print(country_tourist_df.head())
+
+#changing the datatype
+for i in range(1, len(country_tourist_df.columns)):
+    col = country_tourist_df.columns[i]
+    country_tourist_df.iloc[:, i] = pd.to_numeric(country_tourist_df.iloc[:, i],
+                                                  errors='coerce').fillna(0).astype('Int64')
 
 #getting the summary of df
 a= country_tourist_df.info(verbose = True)
@@ -13,23 +19,15 @@ print(a)
 #check the df datatype
 print(country_tourist_df.dtypes)
 
-#changing the datatype
-for i in range(1, len(country_tourist_df.columns)):
-    col = country_tourist_df.columns[i]
-    country_tourist_df.iloc[:, i] = pd.to_numeric(country_tourist_df.iloc[:, i], errors='coerce').fillna(0).astype('Int64')
-print(country_tourist_df.dtypes) #check the updated datatypes
-
 #rename the first index
-country_tourist_df.columns.values[0] = "year_month"
+country_tourist_df = country_tourist_df.rename(columns={country_tourist_df.columns[0]: "year_month"})
 print(country_tourist_df)
 
 #splitting the year and month different
 date = country_tourist_df["year_month"].str.split(' ', n = 2, expand = True)
-
 country_tourist_df["year"] = date[1]
 country_tourist_df["month"] = date[2]
-
-country_tourist_df.head()
+print(country_tourist_df.head())
 
 #sum
 
@@ -38,3 +36,4 @@ country_tourist_df.head()
 
 
 #filter (select) column in region
+
